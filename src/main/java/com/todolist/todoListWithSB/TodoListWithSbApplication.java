f33fd37a25723fc3f013e8c0fdfb89fba58f6094
Данил <dan.kesov.2015@gmail.com>
2021-09-19 01:38:09 +0300

    TaskTracker commit#2

---
+++ src/main/java/com/todolist/todoListWithSB/TodoListWithSbApplication.java
@@ -0,0 +1,13 @@
+package com.todolist.todoListWithSB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListWithSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListWithSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todolist/todoListWithSB/TodoListWithSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todolist.todoListWithSB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListWithSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
